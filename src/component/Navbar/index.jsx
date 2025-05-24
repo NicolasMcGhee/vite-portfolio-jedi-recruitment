26ffcecf842fb2fcccf150620ef34aa5a4ee7110
EternalSuplex <32345511+Skeletonsarecool@users.noreply.github.com>
2025-05-24 08:22:28 -0500

    Added Responsive Navbar

--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import "./index.css";
 import JediSymbol from "../../assets/Symbols/Jedi_symbol.svg";
 import { Link } from "react-router";
 
 export default function Navbar() {
+  const [activeMenu, setActiveMenu] = useState(false);
+
   return (
     <nav className="navbar-Container uppercase">
-      <div className="navbar-Items">
+      <div className="navbar-Home">
         <img src={JediSymbol} alt="Jedi Symbol" width={30} />
         <Link to={"/"}>
-          <p>Join The Jedi</p>
+          <p>Home</p>
         </Link>
       </div>
       <div className="navbar-Items">
@@ -28,6 +30,29 @@ export default function Navbar() {
       </div>
       <div className="navbar-Items ">
         <button className="navbar-Button uppercase">Join Us</button>
+      </div>
+
+      {/* Mobile Button */}
+
+      <button
+        className="mobile-Button"
+        onClick={() => setActiveMenu(!activeMenu)}
+      >
+        {`${activeMenu ? "Close Menu" : "Open Menu"}`}
+      </button>
+      <div className={`${activeMenu == false ? "hidden" : "show"}`}>
+        <ul className="mobile-List">
+          <Link to={"/council-members"}>
+            <li>Council Members</li>
+          </Link>
+          <Link>
+            <li>Stories</li>
+          </Link>
+
+          <Link>
+            <li>Requirements</li>
+          </Link>
+        </ul>
       </div>
     </nav>
   );
